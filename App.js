f3dd5a6310c505b45cb8c40052b59bc8f88249cc
阪本周平 <github000only@gmail.com>
2021-11-24 15:46:01 +0900

    Firebaseを初期化し、使用するプロダクトの SDK の利用を開始

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFibr6AzDu9S_Yxpe6NU8N-wUUztiJN2g",
+  authDomain: "newexpofirebase2-a3a70.firebaseapp.com",
+  projectId: "newexpofirebase2-a3a70",
+  storageBucket: "newexpofirebase2-a3a70.appspot.com",
+  messagingSenderId: "402736408737",
+  appId: "1:402736408737:web:cf097fe991c16e06afe8e0",
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   return (
@@ -14,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
