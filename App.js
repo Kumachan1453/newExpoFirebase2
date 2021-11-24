b159046a57e289aa3413f8fe96c18da3bee028cd
阪本周平 <github000only@gmail.com>
2021-11-25 07:52:11 +0900

    下記サイトのステップ2を実行（ステップ1は実行済） https://firebase.google.cn/docs/web/setup?sdk_version=v9&hl=ja

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFibr6AzDu9S_Yxpe6NU8N-wUUztiJN2g",
+  authDomain: "newexpofirebase2-a3a70.firebaseapp.com",
+  projectId: "newexpofirebase2-a3a70",
+  storageBucket: "newexpofirebase2-a3a70.appspot.com",
+  messagingSenderId: "402736408737",
+  appId: "1:402736408737:web:cf097fe991c16e06afe8e0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   return (
@@ -14,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
